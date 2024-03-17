f1cd9b8d8d50213e39e0e36eec7d8dc58b76dd18
obscuredusername <zubayrr1011@gmail.com>
2024-03-17 12:48:54 +0500

    bakend

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import './App.css';
 import Main from './comp/main';
 import UAE from './comp/uae';
 import World from './comp/world';
+import Sports from './comp/Sports';
+import Ujobs from './comp/ujob';
+import Tech from './comp/tech';
 
 function App() {
 
@@ -16,6 +19,9 @@ function App() {
           <Route path="/" element={<Main/>} />
          <Route path="/uae" element={<UAE />} />
          <Route path="/world" element={<World />} />
+         <Route path="/Sports" element={<Sports/>} />
+         <Route path="/Ujobs" element={<Ujobs/>} />
+         <Route path="/tech" element={<Tech/>} />
           </Routes>
     </BrowserRouter>
   );
