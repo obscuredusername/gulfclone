414fd5da05332024bcf229e57c806762a6f46795
FurqanAhmad2 <furqana405@gmail.com>
2024-03-19 01:28:29 +0500

    added Changes for Production Build testing x14 Solved

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import UAE from "./components/Uae"
 import Main from "./components/Main"
+import World from "./components/world"
+import Ujobs from "./components/ujob"
+import Tech from "./components/tech"
+import Sports from "./components/Sports"
 
 function App() {
   return (
@@ -10,10 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/uae" element={<UAE />} />
-        {/* <Route path="/world" element={<World />} />
+        <Route path="/world" element={<World />} />
         <Route path="/Sports" element={<Sports />} />
         <Route path="/Ujobs" element={<Ujobs />} />
-        <Route path="/tech" element={<Tech />} /> */}
+        <Route path="/tech" element={<Tech />} />
       </Routes>
     </BrowserRouter>
   );
