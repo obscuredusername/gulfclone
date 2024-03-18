0d0c7575884c03c33c0bb38c79f006da83506fe1
FurqanAhmad2 <furqana405@gmail.com>
2024-03-18 23:57:33 +0500

    added Changes for Production Build testing x5

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 import Main from './comp/main';
-import UAE from './comp/Uae';
+import Uae from './comp/Uae';
 import World from './comp/world';
 import Sports from './comp/Sports';
 import Ujobs from './comp/ujob';
@@ -17,7 +17,7 @@ function App() {
     <BrowserRouter>
         <Routes>
          <Route path="/" element={<Main/>} />
-         <Route path="/uae" element={<UAE />} />
+         <Route path="/uae" element={<Uae />} />
          <Route path="/world" element={<World />} />
          <Route path="/Sports" element={<Sports/>} />
          <Route path="/Ujobs" element={<Ujobs/>} />
