3f4274c71848fd76f1d922fbb1b29de758225e90
FurqanAhmad2 <furqana405@gmail.com>
2024-03-30 15:23:00 +0500

    COnfigured Main Page with UAE data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,11 +10,17 @@ import { getDatabase, ref, onValue } from "firebase/database";
 import { app } from '../firebase/Firebase'; // Adjust the path to Firebase.js if needed
 import CreatePost from './createpost';
 
-function Main() {
- 
+function Main() 
+{
   const [headings, setHeadings] = useState([]);
   const [subHeadingPosts, setSubHeadingPosts] = useState([]);
   const [simplePosts, setSimplePosts] = useState([]);
+  const [threePost1, setthreePost1] = useState([]);
+  const [threePost2, setthreePost2] = useState([]);
+  const [threePost3, setthreePost3] = useState([]);
+  const [threePost4, setthreePost4] = useState([]);
+
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -31,7 +37,11 @@ function Main() {
             // Organize posts into separate arrays based on tags
             const headingsArray = [];
             const subHeadingPostsArray = [];
-            const simplePostsArray = [];
+            const simplePostsArray=[];
+            const threeP1 = [];
+            const threeP2 = [];
+            const threeP3 = [];
+            const threeP4 = [];
 
             Object.keys(data).forEach(key => {
               const post = {
@@ -45,15 +55,40 @@ function Main() {
               } else if (post.tags === "Simple Post") {
                 simplePostsArray.push(post);
               }
+              else if (post.tags === "Three Post 1") {
+                threeP1.push(post);
+              }
+              else if (post.tags === "Three Post 2") {
+                threeP2.push(post);
+              }
+              else if (post.tags === "Three Post 3") {
+                threeP3.push(post);
+              }
+              else if (post.tags === "Three Post 4") {
+                threeP4.push(post);
+              }
+
+
             });
 
             setHeadings(headingsArray);
             setSubHeadingPosts(subHeadingPostsArray);
             setSimplePosts(simplePostsArray);
+            setthreePost1(threeP1);
+            setthreePost2(threeP2);
+            setthreePost3(threeP3);
+            setthreePost4(threeP4);
+
+
+
           } else {
             setHeadings([]);
             setSubHeadingPosts([]);
             setSimplePosts([]);
+            setthreePost1([]);
+            setthreePost2([]);
+            setthreePost3([]);
+            setthreePost4([]);
           }
           setLoading(false);
         });
@@ -69,16 +104,24 @@ function Main() {
   console.log("Render - Headings:", headings);
   console.log("Render - Sub Heading Posts:", subHeadingPosts);
   console.log("Render - Simple Posts:", simplePosts);
+  console.log("Render - Simple Posts 1:", threePost1);
+  console.log("Render - Simple Posts 2:", threePost2);
+  console.log("Render - Simple Posts 3:", threePost3);
+  console.log("------------------------<>-------------------------");
 
   return (
     <>
       <Navbar navItems={navItems} />
       <Header title={'Sharjha'} />
       <Navbar2 navItem2={navItem2} />
-       <Navbar3 navItem3={navItem3} /> 
+      {/* <Navbar3 navItem3={navItem3} /> */}
       {/* Render Bodycont only if posts have been fetched */}
-      {loading ? <p>Loading...</p> : <Bodycont headings={headings} subHeadingPosts={subHeadingPosts} simplePosts={simplePosts} />}
-      <Footer/>
+      {loading ? <p>Loading...</p> : <Bodycont headings={headings} 
+      subHeadingPosts={subHeadingPosts} simplePosts={simplePosts} 
+      threePosts1={threePost1} 
+      threePosts2={threePost2} 
+      threePosts3={threePost3} 
+      />}
     </>
   );
 }
